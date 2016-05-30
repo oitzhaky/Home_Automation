639d11fbd6908a2df9ff6ffae8813da34a2879d4
oshriitzhaky <oshri.itzhaky@gmail.com>
2016-05-31 00:13:15 +0300

    IR Remote Commands
    Now can add and remove buttons for future automation

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,40 @@
 from flask import Flask, request, send_from_directory
 from peewee import *
 from datetime import datetime
+import serial
 
 DATABASE = 'homeapp.db'
 
 db = SqliteDatabase(DATABASE)
 
-class BaseModel(Model): 
-    class Meta: 
-        database = db 
 
-class Timer(BaseModel): 
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Timer(BaseModel):
     id
-    set_time = DateTimeField() 
+    set_time = DateTimeField()
     end_time = DateTimeField()
- 
-    class Meta: 
+
+    class Meta:
         order_by = ('end_time',)
 
+
+class Command(BaseModel):
+    id
+    name = CharField()
+
+    class Meta:
+        order_by = ('name',)
+
+
 def create_tables():
-    db.connect()
+    db.connect()  # close?
     db.create_tables([Timer])
+    db.create_tables([Command])
+
 
 app = Flask(__name__, static_url_path='')
 from app import views
--- app/views.py
+++ app/views.py
@@ -5,12 +5,20 @@ import threading
 from datetime import datetime
 from datetime import timedelta
 import serial
-from gcm import GCM 
+from gcm import GCM
 
 global registrationIds
 registrationIds = []
 global API_KEY
 API_KEY = "AIzaSyCZ31BrLMpWpdwsKG9nqKQnDpLoERZnvEc"
+
+
+# ser = serial.Serial()
+# # ser.port = '\\.\COM5'
+# ser.port = "COM5"
+# ser.baudrate= 9600
+# ser.timeout = 2
+# ser.open()
 
 @app.route('/push')
 def push():
@@ -22,45 +30,50 @@ def push():
     global registrationIds
     reg_ids = registrationIds
     response = gcm.json_request(registration_ids=reg_ids, data=data)
-    
+
     return "pushed :-)"
 
+
 def writeToFile(s):
-	 file_path = os.path.normpath('app/static/file.txt')
-	 file = open(file_path, 'w')
-	 file.write(s)
-	 num = file.close()
-	 print(num)
+    file_path = os.path.normpath('app/static/file.txt')
+    file = open(file_path, 'w')
+    file.write(s)
+    num = file.close()
+    print(num)
+
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home Page')
 
-@app.route('/addCommand')
-def addCommand():
-    return render_template('addCommand.html', title='Add Command')
 
 @app.route('/lightsOn', methods=['POST'])
 def lightsOn():
-	writeToFile("1")
-	return "lights on"
+    writeToFile("1")
+    return "lights on"
+
 
 @app.route('/lightsOff', methods=['POST'])
 def lightsOff():
-	writeToFile("0")
-	return "lights off"
+    writeToFile("0")
+    return "lights off"
+
 
 @app.route('/playMusic', methods=['POST'])
 def turnMusicOn():
     file_path = os.path.normpath('app/static/music.mp3')
     os.startfile(file_path)
-    return "playing music..."
+    # return "playing music..."
+
 
 @app.route('/timers')
 def timers():
+    timer = Timer.delete().where(Timer.end_time < datetime.now())
+    timer.execute()
     return render_template('timers.html', title='Timers', timers=Timer.select())
 
-@app.route('/setTimer', methods = ['POST'])
+
+@app.route('/setTimer', methods=['POST'])
 def setTimer():
     str_time = request.form['time']
     hours = int(str_time[:-3])
@@ -71,9 +84,10 @@ def setTimer():
     threading.Timer(time_to_wait_in_seconds, turnMusicOn).start()
     return render_template('timers.html', title='Timers', timers=Timer.select())
 
-@app.route('/subscribe', methods = ['POST', 'GET'])
+
+@app.route('/subscribe', methods=['POST', 'GET'])
 def subscribe():
-    if request.method=='POST':
+    if request.method == 'POST':
         endpoint = request.form['endpoint']
         if endpoint.startswith('https://android.googleapis.com/gcm/send'):
             endpointParts = endpoint.split('/')
@@ -81,14 +95,60 @@ def subscribe():
             global registrationIds
             if registrationIds.count(registrationId) == 0:
                 registrationIds.append(registrationId)
-            #threading.Timer(10, pushNotification).start()
-            #endpoint = 'https://android.googleapis.com/gcm/send'
+                # threading.Timer(10, pushNotification).start()
+                # endpoint = 'https://android.googleapis.com/gcm/send'
         print(len(registrationIds))
         print(registrationId)
         return "registration succeeded"
     else:
         return render_template('subscribe.html', title='Subscribe')
-        
-@app.route('/listeningToArduino', methods=['POST'])
-def listeningToArduino():
-    return render_template('listeningToArduino.html', title='Listening to Arduino')
+
+
+@app.route('/addCommand')
+def addCommand():
+    return render_template('addCommand.html', title='Listening to Arduino', commands=Command.select())
+
+
+@app.route('/receiveCommandFromArduino', methods=['POST'])
+def recieveCommandFromArduino():
+    #    ser.write(b'r')
+    #
+    #    count = ser.readline().decode("utf-8")  #reads first line
+    #    print ('Arduino says:', count)
+    #    time.sleep(3)
+    #    dataSize = ser.readline() #reads 1 bytes(size of data)
+    #    print('Data Size is:', int(dataSize))
+    #
+    #    data = []
+    #    x = 1
+    #    while x <= int(dataSize):
+    #        data.append(ser.readline().strip())
+    #        x = x + 1
+    #
+    #    print('unsigned int raw[',end="")
+    #    print(int(dataSize),end="")
+    #    print('] = ',end="")
+    #    print('{ ',end="")
+    #    for x in range(int(dataSize)-1):
+    #        print(data[x], end="")
+    #        print(',',end="")
+    #    print(' };')
+
+    command_name = request.form['command']
+    Command.create(name=command_name)
+    return render_template('addCommand.html', title='Listening to Arduino', commands=Command.select())
+
+
+@app.route('/sendCommandToArduino', methods=['POST'])
+def sendCommandToArduino():
+    command_id = request.form['id']
+    command = Command.get(Command.id == command_id)
+    return "Command Sent"
+
+
+@app.route('/deleteArduinoCommand', methods=['POST'])
+def deleteArduinoCommand():
+    command_id = request.form['id']
+    command = Command.delete().where(Command.id == command_id)
+    command.execute()
+    return "command deleted"
